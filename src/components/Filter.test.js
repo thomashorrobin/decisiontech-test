774786fa5fd12382149b2aa7fad6af8198b63e94
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2017-05-30 22:38:00 +0100

    updated UI tests

--- src/components/Filter.test.js
+++ src/components/Filter.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Table from './Table';
+import Filter from './Filter';
 const deals = require('./../../public/deals.json').deals;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Table deals={deals} />, div);
+  ReactDOM.render(<Filter setFilter={(filterOptions) => {}} />, div);
 });
